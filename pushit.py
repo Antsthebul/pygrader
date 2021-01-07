2198ed2522561def31f1210adbe4a87dcb63b513
Antsthebul <anthony.allen.srt@gmail.com>
2021-01-07 10:06:42 -0500

    Added home dir optoin to pushit.Imroving logic on changing dirs and echo repr

--- pushit.py
+++ pushit.py
@@ -1,14 +1,19 @@
+import os
+
+from pathlib import Path
 from contextlib import ContextDecorator
 from collections import deque
-import os 
+
+
 
 class pushit(ContextDecorator):
-
+    home = os.environ['HOMEPATH']
     q = deque()
     
     def __init__(self,*args, **kwargs): # init for cntxt mgr
-        if isinstance(args, function): 
-            self.fn = args
+        if args:
+            if type(args[0])== 'function': 
+                self.fn = args
         else:
             pass
         self.q = pushit.q
@@ -17,17 +22,25 @@ class pushit(ContextDecorator):
         print('Init: ', args)
         
     
-    def __call__(self,*args, **kwargs):
-        if args:
+    def __call__(self,file=None, ch_dir=True,**kwargs):
+        if file:
+            if '~' in file:
+                file = Path(pushit.home) / file.lstrip('~/')
             if 'right' not in kwargs:
-                for arg in args:
-                    pushit.q.appendleft(arg)
+                pushit.q.appendleft(file)
             else:
-                for arg in args:
-                    pushit.q.append(arg)
-        print('call: ',args)
-    def __enter__(self):
-        self.fn(file=None)
+                pushit.q.append(file)
+        print('call: ',file)
+        if ch_dir and file:
+            os.chdir(file)
+    
+    def __enter__(self, file=None):
+        self.prev_dir = os.getcwd()
+        print(self.prev_dir)
+        if file: 
+            route = pop(file)
+            os.chdir(route)
+        print('here: ', os.getcwd())
         return pushit.q
 
     def __exit__(self, *exc):
@@ -36,33 +49,68 @@ class pushit(ContextDecorator):
         return False 
     
     def __repr__(self):
+        current = os.getcwd()
+        if not pushit.q:
+            print('0 {};'.format(current))
+
         for ix, obj in enumerate(pushit.q):
+            if  ix== 0:
+                print('{} {};'.format(current))
             print(ix,obj)
         return ''
     
 
 @pushit 
 def pushd(file): # 1st initializes func
-    self.prev_dir = os.getcwd()
-    print(self.prev_dir)
-    if file: os.chdir(file)
-    print('here: ', os.getcwd())
     pass
 
-# pushd()
-# pushd('test') # these are sent to __call__
-# pushd('test/test1')
-# pushd('test/test2', right=True)
-# print('--Start--')
+def popd(file=None):
+    if file:
+        try:
+            pushit.q.remove(file)
+        except ValueError:
+            print(e)
+            print('Unable to remove " %s" Pushd has no more dirs in queue' % file)
+    else:
+        try:
+            pushit.q.popleft()
+        except ValueError:
+            print(e)
+            print('Pushd has no more dirs in queue')
+
+# dirs can be traced from home or by cwd only
+print('Echo:\n',pushd)
+print('-' * 50)
+print('Empty call:\n',pushd())
+print('-' * 50)
+
+print('Single file addition:')
+pushd('test') # these are sent to __call__
+print('Echo:\n',pushd)
+print('-' * 50)
+
+print('Single file addition:')
+pushd('test1') # 
+print('Echo:\n',pushd)
+print('-'*50)
+
+print('Append right: ', pushd('test2', right=True))
+print('Echo:\n',pushd)
+print('-'*50)
+print('From home dir', pushd('~/anaconda3'))
+print('Echo:\n',pushd)
+
+print('-'*50)
+print('--Start--')
 # print(pushd.q)
 
-# print('\n--Context mgr--')
-# print('CM--withoutargs')
-# with pushit() as f:
-#     print(f)
-#     for obj in f:
-#         print(obj)
-#     print('dang\n') 
+print('\n--Context mgr--')
+print('CM--withoutargs')
+with pushit() as f:
+    print(f)
+    for obj in f:
+        print(obj)
+    print('dang\n') 
 
 # print('\nC.M--With args')
 # with pushit('who') as f:
